6ae96bf6958fa8cf4522ac6824a6a1062c12a523
ArthurChan93 <155433642+ArthurChan93@users.noreply.github.com>
2024-04-18 21:39:34 +0800

    Update 01_Service_Income.py

--- pages/01_Service_Income.py
+++ pages/01_Service_Income.py
@@ -95,9 +95,8 @@ else:
     # 其他过滤器组合
     filtered_df = pd.DataFrame(columns=df.columns)  # 创建空的 DataFrame
     
-if invoice_fy_filter:
-    filtered_df = pd.DataFrame(columns=df.columns)
-    filtered_df = filtered_df.append(df_invoice_fy)
+    if invoice_fy_filter:
+        filtered_df = filtered_df.append(df_invoice_fy)
     
     if invoice_yr_filter:
         if not invoice_fy_filter:
